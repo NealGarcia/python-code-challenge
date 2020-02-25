291ed2fdfef2d4cbc8258a55ce2fce6e7e4d73af
Esin Saribudak <esin.saribudak@gmail.com>
2020-02-25 12:48:18 -0600

    Create challenges.py

---
+++ challenges.py
@@ -0,0 +1,23 @@
+# Shortest Word
+
+def shortest_word(string): 
+  # your lovely code here!
+
+print(shortest_word('My name is Inigo Montoya, you killed my father, prepare to die!'))
+  # should return: 2
+  
+
+# Sum of Minimums
+
+def sum_of_minimums(list):
+  # your lovely code goes here!
+
+my_list = [
+    [1,2,3,4,5], # minimum value of row is 1
+    [5,6,7,8,9], # minimum value of row is 5
+    [20,21,34,56,100] # minimum value of row is 20
+    ]
+
+print(sum_of_minimums(my_list))
+  # should return 26
+  
